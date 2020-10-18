b97859e78b60251565eef177aee71ef521424d31
dantxal <dantxal@gmail.com>
2020-10-18 13:23:40 -0300

    Finished workshop 01

--- workshop/01-fetchGraphql/src/App.tsx
+++ workshop/01-fetchGraphql/src/App.tsx
@@ -1,22 +1,54 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import { Flex, Text } from 'rebass';
 import { Card, Content, Button } from '@workshop/ui';
 
+import { fetchGraphQL } from './fetchGraphQL';
+
 const App = () => {
-  const posts = [];
-  const error = false;
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
 
   /**
    * @TODO
    * Fetch posts to be rendered in this component
    */
+  const fetchQuery = useCallback(async () => {
+    try {
+      const response = await fetchGraphQL(
+        `
+      query PostQuery {
+        posts(first:10) {
+          edges {
+            node {
+              id
+              content
+            }
+          }
+        }
+      }
+    `,
+        {},
+      );
+
+      const result = response.data.posts.edges.map(({ node }) => node);
+      setPosts(result);
+    } catch (err) {
+      setError(err.toString());
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchQuery();
+  }, [fetchQuery]);
 
   if (error) {
     return (
       <Content>
         <Text>Error: {error}</Text>
-        <Button mt='10px'>retry</Button>
+        <Button mt='10px' onClick={fetchQuery}>
+          retry
+        </Button>
       </Content>
     );
   }
--- workshop/01-fetchGraphql/src/config.tsx
+++ workshop/01-fetchGraphql/src/config.tsx
@@ -1,6 +1,11 @@
+type Config = {
+  GRAPHQL_URL: string;
+  SUBSCRIPTION_URL: string;
+};
+
 const config = {
   GRAPHQL_URL: process.env.GRAPHQL_URL,
   SUBSCRIPTION_URL: process.env.SUBSCRIPTION_URL,
 };
 
-export default config;
+export default config as Config;
