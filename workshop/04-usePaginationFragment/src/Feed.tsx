44fe5aed01507045bcfd2d2356dc172cc06aebe4
dantxal <dantxal@gmail.com>
2020-10-18 13:31:09 -0300

    Finished Workshop 04

--- workshop/04-usePaginationFragment/src/Feed.tsx
+++ workshop/04-usePaginationFragment/src/Feed.tsx
@@ -1,11 +1,16 @@
 import React, { useCallback } from 'react';
 
 import { Flex } from 'rebass';
-import { Button } from '@workshop/ui';
+
+import { usePaginationFragment, graphql } from 'react-relay/hooks';
+
+import InfiniteScroll from 'react-infinite-scroller';
 
 import Post from './Post';
 
 import { Feed_query } from './__generated__/Feed_query.graphql';
+
+import Loading from './Loading';
 
 type Props = {
   query: Feed_query;
@@ -16,27 +21,58 @@ const Feed = (props: Props) => {
    * TODO
    * usePaginationFragment to fetch posts and paginate
    */
-  const data = {
-    posts: {
-      edges: [],
-    },
-  };
+  const { data, isLoadingNext, loadNext } = usePaginationFragment(
+    graphql`
+      fragment Feed_query on Query
+        @argumentDefinitions(first: { type: "Int", defaultValue: 10 }, after: { type: "String" })
+        @refetchable(queryName: "FeedPaginationQuery") {
+        posts(first: $first, after: $after) @connection(key: "Feed_posts", filters: []) {
+          endCursorOffset
+          startCursorOffset
+          count
+          pageInfo {
+            hasNextPage
+            hasPreviousPage
+            startCursor
+            endCursor
+          }
+          edges {
+            node {
+              id
+              ...Post_post
+            }
+          }
+        }
+      }
+    `,
+    props.query,
+  );
   const { posts } = data;
 
   /**
    * TODO
    * fix loadMore callback to loadMore posts
    */
-  const loadMore = useCallback(() => {}, []);
+  const loadMore = useCallback(() => {
+    if (isLoadingNext) {
+      return;
+    }
+    loadNext(10);
+  }, [isLoadingNext, loadNext]);
 
   return (
     <Flex flexDirection='column'>
-      {posts.edges.map(({ node }) => (
-        <Post key={node.id} post={node} />
-      ))}
-      <Button mt='10px' onClick={loadMore}>
-        Load More
-      </Button>
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={loadMore}
+        hasMore={posts.pageInfo.hasNextPage}
+        loader={<Loading />}
+        useWindow
+      >
+        {posts.edges.map(({ node }) => (
+          <Post key={node.id} post={node} />
+        ))}
+      </InfiniteScroll>
     </Flex>
   );
 };
